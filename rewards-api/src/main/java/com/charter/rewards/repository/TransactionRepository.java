29c27a1900dc94f9aaca926d1e8bfade579a587f
Gopi Reddy <gopireddygopu77@gmail.com>
2026-04-16 22:31:49 +0530

    removing unused method from repository class

--- rewards-api/src/main/java/com/charter/rewards/repository/TransactionRepository.java
+++ rewards-api/src/main/java/com/charter/rewards/repository/TransactionRepository.java
@@ -12,8 +12,6 @@ import java.util.List;
 @Repository
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
 
-    List<Transaction> findByCustomerId(Long customerId);
-
     List<Transaction> findByCustomer_IdAndTransactionDateBetween(Long customerId,
             LocalDateTime from,
             LocalDateTime to);
